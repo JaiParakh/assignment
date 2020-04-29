e5327e7a0fb1a35930c0fb46a6c3f90eafb71b6f
Jai Parakh <jaiparakh.kota.10@gmail.com>
2020-04-29 16:10:54 +0530

    LoginForm and RegisterForm frontend Complete

---
+++ src/components/login-form/Loginform.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import logo from './../../media/logo.png';
+import './Loginform.css';
+//import axios from 'axios';
+
+class LoginForm extends Component{
+    constructor(props){
+        super();
+        
+        this.state = {
+            email: '',
+            password: ''
+        }
+
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+
+    handleEmailChange = (e) => {
+        this.setState({email: e.target.value});
+    }
+
+    handlePasswordChange = (e) => {
+        this.setState({password: e.target.value});
+        console.log(this.state.password)
+    }
+
+    submitData = (e) => {
+        e.preventdefault();
+        const user = {
+            email: this.state.email,
+            password: this.state.password
+        }
+        console.log(user);
+    }
+
+    render(){
+        return(
+            <div>
+                <div className="login-clean">
+                <form method="post" onSubmit={this.submitData}>
+                    <div className="illustration">
+                        <img src={logo} alt="logo" />
+                    </div> 
+                    <div className="form-group">
+                        <input className="form-control" type="email" name="email" placeholder="Email" required onChange={this.handleEmailChange} value={this.state.email} />
+                    </div>
+                    <div className="form-group">
+                        <input className="form-control" type="password" name="password" placeholder="Password" required onChange={this.handlePasswordChange} value={this.state.password}/>
+                    </div>
+                    <div className="form-group">
+                        <input className="btn btn-primary btn-block" value="Log In" type="submit" />
+                    </div>
+                    
+                </form>
+            </div>
+            </div>
+        )
+    }
+}
+
+export default LoginForm;
---
+++ src/components/register-form/RegisterForm.js
@@ -0,0 +1,85 @@
+import React, {Component} from 'react';
+import {Route, Link} from 'react-router-dom';
+import './RegisterForm.css';
+
+class RegisterForm extends Component{
+    constructor(props){
+        super();
+
+        this.state = {
+            email: '',
+            password: '',
+            confirmPassword: '',
+            organisation: '',
+            name: ''
+        }
+    }
+
+    handleEmailChange = (e) => {
+        this.setState({email: e.target.value});
+    }
+
+    handlePasswordChange = (e) => {
+        this.setState({password: e.target.value});
+    }
+
+    handlePassRepChange = (e) => {
+        this.setState({confirmPassword: e.target.value});
+    }
+
+    handleOrganisationChange = (e) => {
+        this.setState({organisation: e.target.value});
+    }
+
+    handleNameChange = (e) => {
+        this.setState({name: e.target.value});
+    }
+
+    submitData = (e) => {
+        e.preventDefault();
+    }
+
+    render(){
+        return(
+            <div>    
+                <div className="register-photo">
+                    <div className="form-container">
+                        <div className="image-holder"></div>
+                        <form onSubmit={this.submitData}>
+                            <h2 className="text-center"><strong>Create</strong> an account.</h2>
+                            <div className="form-group">
+                                    <input className="form-control" type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleEmailChange} required />
+                                </div>
+                            <div className="form-group">
+                                    <input className="form-control" type="text" name="name" placeholder="Name"  onChange={this.handleNameChange} required/>
+                                </div>
+                            <div className="form-group">
+                                    <input className="form-control" type="password" name="password" placeholder="Password"  onChange={this.handlePasswordChange} required/>
+                                </div>
+                            <div className="form-group">
+                                    <input className="form-control" type="password" name="password-repeat" placeholder="Password (repeat)"  onChange={this.handlePassRepChange} required/>
+                                </div>
+                            <div className="form-group">
+                                    <input className="form-control" type="text" name="organisation" placeholder="Organisation Name" onChange={this.handleOrganisationChange} />
+                                </div>
+                            <div className="form-group">
+                                    <div className="form-check">
+                                        <label className="form-check-label">
+                                            <input className="form-check-input" type="checkbox" />I agree to the license terms.
+                                        </label>
+                                    </div>
+                                </div>
+                            <div className="form-group">
+                                    <input type="submit" className="btn btn-primary btn-block" value="Sign Up" />
+                                </div>
+                            <Link className="already" to="/login">Already have an account? Login here.</Link>    
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default RegisterForm;
+
